4b16234db2bce8788b3a67fe9fd22677f96f9a23
Your name <arieftaufikrahman123@gmail.com>
2024-09-17 09:35:20 +0700

    Http Response

---
+++ src/user/user/user.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Controller,
+  Post,
+  Get,
+  Query,
+  Param,
+  Res,
+  Header,
+  HttpCode,
+  HttpRedirectResponse,
+  Redirect,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Controller('/api/users')
+export class UserController {
+  @Get('/sample-response')
+  @Header('Content-Type', 'application/json')
+  @HttpCode(200)
+  sampleResponse(): Record<string, string> {
+    return {
+      data: 'Hello JSON',
+    };
+  }
+  @Get('/redirect')
+  @Redirect()
+  redirect(): HttpRedirectResponse {
+    return {
+      url: '/api/users/sample-response',
+      statusCode: 301,
+    };
+  }
+
+  @Get('/hello')
+  sayHello(
+    @Query('first_name') firstName: string,
+    @Query('last_name') lastName: string,
+  ): string {
+    return `Hello ${firstName} ${lastName}!`;
+  }
+
+  @Get('/:id')
+  getById(@Param('id') id: string): string {
+    return `GET ${id}`;
+  }
+
+  @Post()
+  post(): string {
+    return 'POST';
+  }
+
+  @Get('/sample')
+  get(): string {
+    return 'Hello NestJS';
+  }
+}
