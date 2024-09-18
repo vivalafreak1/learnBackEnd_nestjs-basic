fb8473e69a0db3f3e75b4f8861951a6d814c6673
Eddiesti <51298768+vivalafreak1@users.noreply.github.com>
2024-09-18 20:59:32 +0700

    Custom Provider

--- src/user/user/user.controller.spec.ts
+++ src/user/user/user.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import * as httpMock from 'node-mocks-http';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -9,7 +10,7 @@ describe('UserController', () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
       imports: [],
-      providers: [],
+      providers: [UserService],
     }).compile();
 
     controller = module.get<UserController>(UserController);
@@ -20,8 +21,8 @@ describe('UserController', () => {
   // });
 
   it('should can say hello', async () => {
-    const response = await controller.sayHello('arief', 'taufik');
-    expect(response).toBe('Hello arief taufik!');
+    const response = await controller.sayHello('arief');
+    expect(response).toBe('Hello arief!');
   });
 
   // Testing express.Response using mock
--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -10,11 +10,37 @@ import {
   Redirect,
   Res,
   Req,
+  Inject,
 } from '@nestjs/common';
 import { Response, Request } from 'express';
+import { UserService } from './user.service';
+import { Connection } from '../connection/connection';
+import { MailService } from '../mail/mail.service';
+import { UserRepository } from '../user-repository/user-repository';
 
 @Controller('/api/users')
 export class UserController {
+  constructor(
+    private service: UserService,
+    private connection: Connection,
+    private mailService: MailService,
+    @Inject('EmailService') private emailService: MailService,
+    private userRepository: UserRepository,
+  ) {}
+
+  @Get('/connection')
+  async getConnection(): Promise<string> {
+    this.userRepository.save();
+    this.mailService.send();
+    this.emailService.send();
+    return this.connection.getName();
+  }
+
+  @Get('/hello')
+  async sayHello(@Query('name') name: string): Promise<string> {
+    return this.service.sayHello(name);
+  }
+
   @Get('/view/hello')
   viewHello(@Query('name') name: string, @Res() response: Response) {
     response.render('index.html', {
@@ -49,14 +75,6 @@ export class UserController {
       url: '/api/users/sample-response',
       statusCode: 301,
     };
-  }
-
-  @Get('/hello')
-  async sayHello(
-    @Query('first_name') firstName: string,
-    @Query('last_name') lastName: string,
-  ): Promise<string> {
-    return `Hello ${firstName} ${lastName}!`;
   }
 
   @Get('/:id')
